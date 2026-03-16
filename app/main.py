8fc7f7aeeb0b02ac12f2f5084b8b33cb8e263b34
Anja Scherwall <anjagutt@hotmail.com>
2026-03-16 11:56:53 +0100

    feat: typehint and new get
    Updated get for getting temperature for a specific date with typehint on values. Created a ner app.get for getting all dates with temperatures in the database

--- app/main.py
+++ app/main.py
@@ -1,47 +1,67 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
 import os
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
 from databases import Database
-import asyncpg
 
 # Get the information in .env-file
 load_dotenv()
-# Create the connection to database
-DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
-database = Database(DATABASE_URL)
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    database_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+
+    # Create a connection to the database
+    app.state.database = Database(database_url)
+
+    # Connect to database
+    await app.state.database.connect()
+    print("Database connected")
+
+    yield
+
+    # Shutdown
+    await app.state.database.disconnect()
+    print("Database disconnected")
+
+
+app = FastAPI(lifespan= lifespan)
 
 #
 @app.get("/")
 def root():
     return {"message": "Hello Weather"}
 
-# Start the connection to the database when container starts
-@app.on_event("startup")
-async def startup():
-    await database.connect()
 
-# Close the connection to the database when docker is shut down
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+@app.get("/all_dates")
+async def all_dates():
+    # Get all the dates/rows in database
+    query = "SELECT * FROM weather_data ORDER BY date"
+    result = await app.state.database.fetch_all(query=query)
 
+    # Create a list with dicts (for each row), returns list to user
+    return {
+        "all_temperatures": [
+            {"date": row["date"], "temperature": row["temperature"]}
+            for row in result
+        ]
+    }
 
-@app.get("/date_temperature/{date}")
+@app.get("/date_temp/{date}")
+# When sending a request, FastAPI extracts date as a str
 async def date_temperature(date: str):
     # Testing to see if date is in database
     check_query = "SELECT COUNT(*) as count FROM weather_data WHERE date = :date"
-    values= {"date": date}
+    values: dict[str, str]= {"date": date}
 
-    count_result = await database.fetch_one(query=check_query, values=values)
+    count_result = await app.state.database.fetch_one(query=check_query, values=values)
     # If date not in database, return message
     if count_result["count"] == 0:
         return {"message": f"Date {date} does not exist in database"}
 
     # If date is in database, return date and temperature
     query = "SELECT * FROM weather_data WHERE date = :date"
-    result = await database.fetch_one(query=query, values=values)
+    result = await app.state.database.fetch_one(query=query, values=values)
 
     return {"date": date, "temperature": result["temperature"]}
 
