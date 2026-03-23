d775f266aec264228bd1922bcd8882aa7ef20634
Dennis Chinecherem <densenj@outlook.com>
2026-03-23 14:12:43 +0100

    feat: update sql ingestion script to run in docker

--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,21 +1,15 @@
---- Raw and clean test schemas ---
-CREATE SCHEMA IF NOT EXISTS raw2;
-CREATE SCHEMA IF NOT EXISTS clean2;
+-- Create schema
+CREATE SCHEMA IF NOT EXISTS raw;
 
---- Raw table with 2 columns with TEXT type to take in all errors ---
-CREATE TABLE IF NOT EXISTS raw2.chris_table (
-    created_at TEXT,
-    temp TEXT
+-- Drop table if exists
+DROP TABLE IF EXISTS raw.stockholm_raw;
+
+-- Create table
+CREATE TABLE raw.stockholm_raw (
+    line TEXT
 );
 
---- Clean table, same principles ---
-CREATE TABLE IF NOT EXISTS clean2.chris_table2 (
-    created_at DATE,
-    temp      FLOAT
-);
-
---- Bulk ingest into table with path, delimeter and header ---
-COPY raw2.chris_table (created_at, temp)
-FROM '/data/mockup_data.csv'
-DELIMITER ';'
-CSV HEADER;
+-- Load file (must match Docker-mounted path)
+COPY raw.stockholm_raw(line)
+FROM '/data/raw_stockholm.csv'
+WITH (FORMAT text);
